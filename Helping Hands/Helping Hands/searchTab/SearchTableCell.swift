eec133d9bd261feab84d076fe3b4246e35872ff8
Tracy Nguyen <apathetictracyn@gmail.com>
2018-03-17 14:09:03 -0500

    Search page implementation + filtering. Need to connect data.

---
+++ Helping Hands/Helping Hands/searchTab/SearchTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  searchTableCell.swift
+//  Helping Hands
+//
+//  Created by Tracy Nguyen on 3/17/18.
+//  Copyright © 2018 Tracy Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class searchTableCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
