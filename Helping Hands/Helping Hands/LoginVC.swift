810aa0844b1ed683d355cce26c58c8094387d094
manasa884 <manasa.tipparam@gmail.com>
2018-03-18 01:53:15 -0700

    Integration with Firebase, Sign up working

---
+++ Helping Hands/Helping Hands/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  Helping Hands
+//
+//  Created by Manasa Tipparam on 3/18/18.
+//  Copyright © 2018 Tracy Nguyen. All rights reserved.
+//
+
+import Foundation
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
--- Helping Hands/Helping Hands/RegistrationVC.swift
+++ Helping Hands/Helping Hands/RegistrationVC.swift
@@ -7,12 +7,69 @@
 //
 
 import Foundation
-import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class RegistrationVC: UIViewController {
     
+    let databaseRef = FIRDatabase.database().reference(fromURL: "https://helping-hands-8f10c.firebaseio.com/")
+    
     @IBOutlet weak var termsAgreementsCB: BEMCheckBox!
     @IBOutlet weak var over18CB: BEMCheckBox!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBAction func UploadImagePressed(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var firstNameTF: UITextField!
+    @IBOutlet weak var lastNameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var confirmPasswordTF: UITextField!
+    
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+        // Make sure none of the fields are empty
+        // (image is optional, placeholder will be used if none specified)
+        if firstNameTF.text == "" || lastNameTF.text == "" || emailTF.text! == "" || passwordTF.text! == "" || confirmPasswordTF.text == "" {
+            let alert = UIAlertController(title: "Empty Fields!",
+                                          message: "Make sure all the fields are completed",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // Make sure both passwords match
+        if passwordTF.text != confirmPasswordTF.text {
+            passwordTF.text = ""
+            confirmPasswordTF.text = ""
+            return
+        }
+        
+        if let email = emailTF.text, let pass = passwordTF.text {
+            FIRAuth.auth()?.createUser(withEmail: email, password: pass, completion: { user, error in
+                if let firebaseError = error {
+                    print(firebaseError.localizedDescription)
+                    return
+                }
+                print("success!")
+                guard let uid = user?.uid else {
+                    return
+                }
+                let userReference = self.databaseRef.child("users").child(uid)
+                let values = ["firstName": self.firstNameTF.text!, "lastName": self.lastNameTF.text!, "email": self.emailTF.text!, "photo": ""]
+                
+                userReference.updateChildValues(values, withCompletionBlock: { (error, ref) in
+                    if error != nil {
+                        print(error!)
+                        return
+                    }
+                    self.dismiss(animated: true, completion: nil)
+                })
+            })
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
