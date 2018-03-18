ae1d7dac2dbc4e0cb9d91bbe74ee5941ff7571a5
Tracy Nguyen <apathetictracyn@gmail.com>
2018-03-18 01:49:36 -0500

    Search currently working with title as filter.

--- Helping Hands/Helping Hands/searchTab/SearchTabViewController.swift
+++ Helping Hands/Helping Hands/searchTab/SearchTabViewController.swift
@@ -7,27 +7,38 @@
 //
 
 import UIKit
+import CoreData
 
 class SearchTabViewController: UITableViewController, UISearchResultsUpdating {
-    
     
     let unfilteredNFLTeams = ["Bengals", "Ravens", "Browns", "Steelers", "Bears", "Lions", "Packers", "Vikings",
                               "Texans", "Colts", "Jaguars", "Titans", "Falcons", "Panthers", "Saints", "Buccaneers",
                               "Bills", "Dolphins", "Patriots", "Jets", "Cowboys", "Giants", "Eagles", "Redskins",
                               "Broncos", "Chiefs", "Raiders", "Chargers", "Cardinals", "Rams", "49ers", "Seahawks"].sorted()
+    
+    var unfilteredJobs = [NSManagedObject]()
+    var filteredJobs: [NSManagedObject]?
+    
     var filteredNFLTeams: [String]?
     let searchController = UISearchController(searchResultsController: nil)
     
     
     
     override func viewDidLoad() {
-        filteredNFLTeams = unfilteredNFLTeams
+        
+        for job in retrieveJobs() {
+            unfilteredJobs.append(job)
+        }
+        
+        filteredJobs = unfilteredJobs
         searchController.searchResultsUpdater = self
         searchController.hidesNavigationBarDuringPresentation = false
         searchController.dimsBackgroundDuringPresentation = false
         tableView.tableHeaderView = searchController.searchBar
         
         super.viewDidLoad()
+        
+       
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -35,18 +46,22 @@ class SearchTabViewController: UITableViewController, UISearchResultsUpdating {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let nflTeams = filteredNFLTeams else {
+        guard let returnedResults = filteredJobs else {
             return 0
         }
-        return nflTeams.count
+        return returnedResults.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath) as! SearchTableCell
         
-        if let nflTeams = filteredNFLTeams {
-            let team = nflTeams[indexPath.row]
-            cell.textLabel!.text = team
+        if let returnedResults = filteredJobs {
+            let result = returnedResults[indexPath.row]
+            cell.jobTitleLabel.text = result.value(forKey: "jobTitle") as? String
+            cell.picture.image = UIImage(data: result.value(forKey: "jobImage") as! Data)
+            cell.distanceLabel.text = String(result.value(forKey: "jobDistance") as! Double)
+            cell.descriptionLabel.text = result.value(forKey: "jobDescription") as? String
+            
         }
         
         return cell
@@ -54,14 +69,65 @@ class SearchTabViewController: UITableViewController, UISearchResultsUpdating {
     
     func updateSearchResults(for searchController: UISearchController) {
         if let searchText = searchController.searchBar.text, !searchText.isEmpty {
-            filteredNFLTeams = unfilteredNFLTeams.filter { team in
-                return team.lowercased().contains(searchText.lowercased())
+            
+            filteredJobs = unfilteredJobs.filter { job in return ((job.value(forKey: "jobTitle") as? String)?.lowercased().contains(searchText.lowercased()))!
+                
             }
             
         } else {
-            filteredNFLTeams = unfilteredNFLTeams
+            filteredJobs = unfilteredJobs
         }
         tableView.reloadData()
+    }
+    
+    func retrieveJobs() -> [NSManagedObject] {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName:"JobEntity")
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        // Examples of filtering using predicates
+        // let predicate = NSPredicate(format: "age = 35")
+        // let predicate = NSPredicate(format: "name CONTAINS[c] 'ake'")
+        // request.predicate = predicate
+        
+        do {
+            try fetchedResults = context.fetch(request) as? [NSManagedObject]
+        } catch {
+            // If an error occurs
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        return(fetchedResults)!
+        
+    }
+    
+    func retrieveEvents() -> [NSManagedObject] {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName:"EventEntity")
+        var fetchedResults:[NSManagedObject]? = nil
+        
+        // Examples of filtering using predicates
+        // let predicate = NSPredicate(format: "age = 35")
+        // let predicate = NSPredicate(format: "name CONTAINS[c] 'ake'")
+        // request.predicate = predicate
+        
+        do {
+            try fetchedResults = context.fetch(request) as? [NSManagedObject]
+        } catch {
+            // If an error occurs
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        return(fetchedResults)!
+        
     }
 
 }
