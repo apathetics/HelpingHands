0b78293d224a24c68ccf6cdf2631b37ae260a6a7
Bryan Bernal <bryanbernal97@utexas.edu>
2018-03-21 21:34:02 -0500

    Cleaned up contact us dummy

---
+++ Helping Hands/Helping Hands/SideBarMenu/ContactUsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ContactUsViewController.swift
+//  Helping Hands
+//
+//  Created by Bryan Bernal on 3/21/18.
+//  Copyright © 2018 Tracy Nguyen. All rights reserved.
+//
+
+import Foundation
