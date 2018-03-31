55be33ffadc85b00fa17e8d5b0205705a07b47f5
Bryan Bernal <bryanbernal97@utexas.edu>
2018-03-30 23:30:51 -0500

    Got locations to show up as search results, and addresses are "pinnable", though work still needs to be done on getting appropriate labels for each address.

--- Helping Hands/Helping Hands/ExploreTab/LocationViewController.swift
+++ Helping Hands/Helping Hands/ExploreTab/LocationViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 import MapKit
 
-class LocationViewController : UIViewController, CLLocationManagerDelegate {
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
+class LocationViewController : UIViewController, CLLocationManagerDelegate, HandleMapSearch {
+    
     
     @IBOutlet weak var mapView: MKMapView!
     
+    var resultSearchController:UISearchController? = nil
+    var selectedPin:MKPlacemark? = nil
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -21,6 +28,22 @@ class LocationViewController : UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
+        
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Search for places"
+        navigationItem.titleView = resultSearchController?.searchBar
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -39,5 +62,23 @@ class LocationViewController : UIViewController, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Swift.Error) {
         print("error:: (error)")
+    }
+    
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "(city) (state)"
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
     }
 }
