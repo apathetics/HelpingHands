b0dc8e6e99a7a959d7d4bee3ee5984ec5d7ffda1
Bryan Bernal <bryanbernal97@utexas.edu>
2018-03-30 22:50:44 -0500

    Added a Location View Controller that will be used to find a location when posting a job, event, etc. Currently only shows current location on a map view

---
+++ Helping Hands/Helping Hands/ExploreTab/LocationViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LocationViewController.swift
+//  Helping Hands
+//
+//  Created by Bryan Bernal on 3/30/18.
+//  Copyright © 2018 Tracy Nguyen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationViewController : UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Swift.Error) {
+        print("error:: (error)")
+    }
+}
